915f9576cc24bdf9b43220d9902cbf27d4fbd4b3
shivammittal <devoteeofbadrinath@ymail.com>
2025-05-11 14:48:11 +0530

    Message

--- Multithreaded Kernel/PeachOS/boot.asm
+++ Multithreaded Kernel/PeachOS/boot.asm
@@ -1,11 +1,19 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
 start:
+    cli ; Clear interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+    sti ; Enable interrupts
     mov si, message
     call print
     jmp $
-
 
 print:
     mov bx, 0
@@ -20,10 +28,10 @@ print:
 
 print_char:
     mov ah, 0eh
-    int 0x10
+    int ox10
     ret
 
-message db 'Hello, World!', 0
+message: db 'Hello World!', 0
 
-times 510-($-$$) db 0
-    dw 0xAA55
+times 510-($ - $$) db 0
+dw 0xAA55
