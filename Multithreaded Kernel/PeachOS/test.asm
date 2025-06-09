1350753ff8d634424f85c851129dc4d591fad341
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-06-09 22:24:39 +0530

    Message

--- Multithreaded Kernel/PeachOS/test.asm
+++ Multithreaded Kernel/PeachOS/test.asm
@@ -1,3 +1,7 @@
 mov ax, 0x0000
 mov ss, ax       ; SS = 0x0000
 mov sp, 0x7C00   ; SP = 0x7C00
+mov ax, 0x1234
+push ax   ; Decrements SP by 2, then writes AX to [SS:SP]
+mov bx, 0x5678
+push bx   ; Decrements SP by 2, then writes BX to [SS:SP]
