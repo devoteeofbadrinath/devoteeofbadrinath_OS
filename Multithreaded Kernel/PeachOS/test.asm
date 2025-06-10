af5ca9623d52398ab8d61e7a0cfa7bd30e843104
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2025-06-10 11:21:02 +0530

    Message

--- Multithreaded Kernel/PeachOS/test.asm
+++ Multithreaded Kernel/PeachOS/test.asm
@@ -1,7 +1,45 @@
-mov ax, 0x0000
-mov ss, ax       ; SS = 0x0000
-mov sp, 0x7C00   ; SP = 0x7C00
-mov ax, 0x1234
-push ax   ; Decrements SP by 2, then writes AX to [SS:SP]
-mov bx, 0x5678
-push bx   ; Decrements SP by 2, then writes BX to [SS:SP]
+ORG 0
+BITS 16
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
+start:
+    jmp 0x7c0:step2
+
+step2:
+    cli ; Clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+    sti ; Enable Interrupts
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello World!', 0
+
+times 510-($ - $$) db 0
+dw 0xAA55
