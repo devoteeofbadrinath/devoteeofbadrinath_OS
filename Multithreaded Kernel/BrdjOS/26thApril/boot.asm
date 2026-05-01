f3a3b77bfb93c7bd68a72b8d572c8175cd59059f
devoteeofbadrinath <devoteeofbadrinath@ymail.com>
2026-05-01 15:29:55 +0530

    Message

--- Multithreaded Kernel/BrdjOS/26thApril/boot.asm
+++ Multithreaded Kernel/BrdjOS/26thApril/boot.asm
@@ -1,12 +1,16 @@
 ORG 0x7c00
 BITS 16
 
-start:
+start: 
+
+    jmp $
+    
+print:
     mov ah, 0eh
     mov al, 'A'
     int 0x10
 
 message: db 'Hello World!', 0
 
-times 510 - ($-$$) db 0
+times 510 - ($-$$) db 254
 dw 0xAA55
